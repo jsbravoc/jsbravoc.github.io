2dc10be955e2da4e0a3b9d89cea63364fbb51590
Juan Sebastián Bravo Castelo <js.bravo@uniandes.edu.co>
2020-02-02 00:53:49 -0500

    Actualización navbar e ícono
    La navbar ahora incluye íconos de redes sociales, una fuente de texto diferente y animaciones al resaltar un vínculo: 
    Animación de navbar basada en snippet de LittleSnippets.net
      https://codepen.io/littlesnippets/pen/pjmXvP

--- js/hoja-de-vida.js
+++ js/hoja-de-vida.js
@@ -1,1 +1,18 @@
-PDFObject.embed("../assets/file.pdf","#pdfcv");
+
+const verificarMovil = function(){
+    let url = window.location +"";
+    let index = url.lastIndexOf("/pages");
+    if(index>0)
+    {
+        let pdf = url.substring(0,index)+ "/assets/file.pdf";
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        window.location.replace(pdf);
+    }
+    else
+    {
+        console.log("Error: no se encontró la ruta adecuada del archivo PDF")
+    }
+   }
+}
+
+verificarMovil();
