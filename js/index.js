11ad71483ab2d93a82a3c7cc9dec1b53e7ba75d8
Juan Sebastián Bravo Castelo <js.bravo@uniandes.edu.co>
2020-02-03 10:44:40 -0500

    Cambio de font Typed.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     var typed = new Typed('#intro', {
         stringsElement: '#typed-intro',
-        typeSpeed: 15,
-        backSpeed: 15,
-        startDelay: 3000,
+        typeSpeed: 8,
+        backSpeed: 8,
+        startDelay: 2000,
         loop: false,
-        loopCount: Infinity
+        loopCount: 1
     });
     var typed2 = new Typed('#descripcion', {
         strings: ["Soy estudiante", "Soy desarrollador web", "Soy determinado", "Soy autodidacta"],
-        typeSpeed: 15,
-        backSpeed: 15,
-        startDelay: 4000,
+        typeSpeed: 8,
+        backSpeed: 8,
+        startDelay: 2500,
         loop: true,
         loopCount: Infinity
     });
