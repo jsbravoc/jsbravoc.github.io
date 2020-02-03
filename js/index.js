2fd6b90be36f1b3177b5c99d04e39a3425dfec8b
Juan Sebastián Bravo Castelo <js.bravo@uniandes.edu.co>
2020-02-03 10:52:07 -0500

    Prueba Typed-js cambio de cursor

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     var typed = new Typed('#intro', {
         stringsElement: '#typed-intro',
-        typeSpeed: 30,
+        typeSpeed: 35,
         startDelay: 2000,
         loop: false,
         loopCount: 1,
-        onComplete: function(self) { self.cursorChar = '' }
+        cursorChar = ''
     });
     var typed2 = new Typed('#descripcion', {
-        strings: ["Soy estudiante", "Soy desarrollador web", "Soy determinado", "Soy autodidacta"],
-        typeSpeed: 8,
-        backSpeed: 8,
+        strings: ["Soy estudiante", "Soy desarrollador Web", "Soy determinado", "Soy autodidacta"],
+        typeSpeed: 35,
         startDelay: 2500,
         loop: true,
         loopCount: Infinity
