c35285e83409be50f6204ff9df9b0a3805747556
Juan Sebastián Bravo Castelo <js.bravo@uniandes.edu.co>
2020-02-03 00:03:35 -0500

    Implementación de página quién-soy-yo

--- js/hoja-de-vida.js
+++ js/hoja-de-vida.js
@@ -1,18 +1,21 @@
-
-const verificarMovil = function(){
-    let url = window.location +"";
+/*
+Función para cargar el PDF de la Hoja de Vida en un celular. Dado que varios celulares no soportan el elemento <object data=../>
+Se utiliza esta función para que los rediriga (inmediatamente cargue la página) a la descarga del archivo.
+pre: El archivo existe en la ruta y el usuario navega a la página hoja-de-vida.
+post: Si el usuario se encuentra visitando la página desde un celular, se redirigirá a la descarga de la hoja de vida. En caso contrario,
+el navegador mostrará de manera normal el PDF según la integración propia del mismo. 
+ */
+const verificarMovil = function() {
+    let url = window.location + "";
     let index = url.lastIndexOf("/pages");
-    if(index>0)
-    {
-        let pdf = url.substring(0,index)+ "/assets/file.pdf";
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        window.location.replace(pdf);
+    if (index > 0) {
+        let pdf = url.substring(0, index) + "/assets/file.pdf";
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            window.location.replace(pdf);
+        } else {
+            console.log("Error: no se encontró la ruta adecuada del archivo PDF")
+        }
     }
-    else
-    {
-        console.log("Error: no se encontró la ruta adecuada del archivo PDF")
-    }
-   }
 }
 
 verificarMovil();
